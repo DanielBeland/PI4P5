a8bb0c37314df569bb33636eb44e0ecc7f69b503
Olivier ONeel <onopsp@pckswarms.ch>
2019-03-20 15:37:50 -0400

    Update gui_tkinter.py

--- gui_tkinter.py
+++ gui_tkinter.py
@@ -37,13 +37,14 @@ from reportlab.graphics.charts.linecharts import HorizontalLineChart
 from reportlab.graphics.charts.lineplots import LinePlot
 from reportlab.graphics.widgets.markers import makeMarker
 from reportlab.platypus import *
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
-
-
-from PIL import ImageTk, Image
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_CENTER
+from reportlab.graphics.charts.legends import LineLegend
 import time
+import datetime
 starttime=time.time()
 
 
@@ -108,7 +109,6 @@ class StartPage(tk.Frame):
                               command=lambda: PopSettingPage(self))
         button_Setting.pack() 
 
-    
         
     def event(self):
         AcqVerSav_threads.stopThreads(AcqVerSav_threads.thread_list)
@@ -137,30 +137,35 @@ class RecordingPage(tk.Frame):
         self.image = tk.PhotoImage(file='bluetooth.png')
         self.image_disp=self.image.subsample(6,6)
         self.label = tk.Label(self, image=self.image_disp, bg='green',borderwidth=2, relief="solid")
-        self.label.grid(row=0,column=0,pady=10)
-        self.comlist=['com1','com2','com3']
+        self.label.grid(rowspan=2,column=0,pady=10)
+        self.comlist=['1','2','3']
         
         self.v = tk.StringVar()
         self.v.set(self.comlist[0])
         self.BluetoothOption = ttk.OptionMenu(self, self.v, self.comlist[0], *self.comlist)
         self.BluetoothOption.grid(row=0,column=1,pady=10)
+        self.RefreshButton=ttk.Button(self,text='refresh', command=lambda: RefreshBluetooth(self))
+        self.RefreshButton.grid(row=1,column=1,pady=10)
+        
+        
+        
         self.saveLocationText=tk.StringVar()
-        self.saveLocationText.set('File Save Name: ')
+        self.saveLocationText.set('')
         self.saveLocationLabel=ttk.Label(self,textvariable=self.saveLocationText)
-        self.saveLocationLabel.grid(row=1,column=0,pady=10)
+        self.saveLocationLabel.grid(row=2,column=0,pady=10)
         
         self.editButton=ttk.Button(self,text='Edit',command=lambda: EditSaveLocation(self))
-        self.editButton.grid(row=1,column=1,pady=10)
+        self.editButton.grid(row=2,column=1,pady=10)
         
         self.startButton=ttk.Button(self,text='Start',command=lambda: StartRecording(self))
-        self.startButton.grid(row=2,column=0,pady=10)
+        self.startButton.grid(row=3,column=0,pady=10)
         self.stopButton=ttk.Button(self,text='Stop',command=lambda: StopRecording(self))
         self.stopButton.config(state="disabled")
-        self.stopButton.grid(row=2,column=1,pady=10)
+        self.stopButton.grid(row=3,column=1,pady=10)
         
         self.homeButton=ttk.Button(self, text="Home",
                   command=lambda: controller.show_frame(StartPage))
-        self.homeButton.grid(row=3,columnspan=2,pady=10)
+        self.homeButton.grid(row=4,columnspan=2,pady=10)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
@@ -169,6 +174,15 @@ class RecordingPage(tk.Frame):
         state = str(self.startButton['state'])
         if state == 'normal':
             EditSaveLocation(self)
+def RefreshBluetooth(self):
+    self.comlist=[str(np.random.randint(0,4)),str(np.random.randint(0,4)),str(np.random.randint(0,4))]
+    menu = self.BluetoothOption["menu"]
+    menu.delete(0, "end")
+    for string in self.comlist:
+        menu.add_command(label=string, 
+                         command=lambda value=string: self.v.set(value))
+    self.v.set(self.comlist[0])
+
 def CheckState():
     state=[np.random.randint(0,2),np.random.randint(0,2)]
     crise=state[0]
@@ -190,6 +204,7 @@ def StartRecording(self):
         self.stopButton.config(state='enabled')
         self.BluetoothOption.config(state='disabled')
         self.editButton.config(state='disabled')
+        self.homeButton.config(state='disabled')
     else:
         messagebox.showwarning("No file name", "Please enter a save file")
 def StopRecording(self):
@@ -199,18 +214,20 @@ def StopRecording(self):
         self.stopButton.config(state='disabled')
         self.BluetoothOption.config(state='enabled')
         self.editButton.config(state='enabled')
+        self.homeButton.config(state='enabled')
     
     ########### CREATE SUMMARY#################
     
 def Rapport():
     PAGE_HEIGHT=defaultPageSize[1]
     styles=getSampleStyleSheet()
-    Title="Rapport de la session d'enregistrement"
+    Title="<para alignment='center'>Rapport de la session d'enregistrement</para>"
+    logo = "logo.png"
+    formatted_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     Elements=[]
     HeaderStyle=styles["Heading1"]
     CapteurStyle=styles["Heading2"]
     ParaStyle=styles["Normal"]
-    PreStyle=styles["Code"]
     capteur=["ECG","EMG bras droite","EMG bras gauche","EMG jambe droite","EMG jambe gauche","Accéléromètre bras droite","Accéléromètre bras gauche","Accéléromètre jambe droite","Accéléromètre jambe gauche","Respiration","EDA"]
     def header(txt, style=HeaderStyle, klass=Paragraph, sep=0.3):
         s=Spacer(0.2*inch, sep*inch)
@@ -219,22 +236,22 @@ def Rapport():
         result=KeepTogether(sect)
         return result
     def graphout(data):
-        drawing = Drawing(400, 200)
+        drawing = Drawing(400, 210)
         lp = LinePlot()
         lp.x = 50
         lp.y = 50
-        lp.height = 125
-        lp.width = 300
+        lp.height = 150
+        lp.width = 400
         lp.data = data
         lp.joinedLines = 1
-        lp.lines[0].symbol = makeMarker('FilledCircle')
-    #    lp.lines[1].symbol = makeMarker('Circle')
-        lp.lineLabelFormat = '%2.0f'
-        #lp.strokeColor = colors.black
-    #    lp.xValueAxis.valueMin = 0
-    #    lp.xValueAxis.valueMax = 300
-    #    lp.xValueAxis.valueSteps = [1, 2, 2.5, 3, 4, 5]
+        lp.lineLabelFormat = None
+        lp.lines[1].strokeColor=colors.black
+        lp.lines[2].strokeColor=colors.black
+        lp.lines[1].strokeWidth=0.5
+        lp.lines[2].strokeWidth=0.5
         lp.xValueAxis.labelTextFormat = '%2.1f'
+        
+
     #    lp.yValueAxis.valueMin = 0
     #    lp.yValueAxis.valueMax = 1
     #    lp.yValueAxis.valueSteps = [1, 2, 3, 5, 6]
@@ -245,39 +262,70 @@ def Rapport():
         root.withdraw()
     
         file_path = filedialog.askopenfilename()
-        data = np.genfromtxt(file_path, delimiter=',')
-        return [data, file_path]
+        if not file_path:
+            return [0,0]
+        else:
+            data = np.genfromtxt(file_path, delimiter=',')
+            return [data, file_path]
     
     def go(file_path):
         GenName='Rapport'
         PatientName=os.path.basename(file_path)[:-4]
-        SummaryName=GenName+'_'+PatientName+'.pdf'
+        Scan_date=datetime.datetime.fromtimestamp(os.stat(file_path).st_mtime).strftime('%Y_%m_%d')
+        SummaryName=GenName+'_'+PatientName+'_'+Scan_date+'.pdf'
         doc=SimpleDocTemplate(SummaryName)
         doc.build(Elements)
-        
-    myTitle=header(Title)
-    info=[myTitle]
-    Elements.extend(info)
     [data,file_path]=select_file()
-    for i in range(11):
-        Capteur=header(capteur[i], sep=0.1, style=CapteurStyle)
-        meanData=header('Mean='+str(np.mean(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
-        stdData=header('Standard Deviation='+str(np.std(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
-        minData=header('Minimum='+str(np.min(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
-        maxData=header('Maximum='+str(np.max(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
-        ydata=[]
-        xdata=[]
-        data_graph=[]
-    
-            
-        xdata=np.arange(data.shape[0]-1)
-        ydata=data[1:data.shape[0],i]
-        data_graph = [tuple(zip(xdata,ydata))]
-        graph = graphout(data_graph)
-        info_capteur=[Capteur, meanData,stdData,maxData,minData,graph]
-        Elements.extend(info_capteur)
-    
-    go(file_path)
+    if file_path==0:
+        pass
+    else:
+        im = Image(logo, 2*inch, 2*inch)
+        Elements.append(im)
+        Elements.append(Spacer(2*inch, 2*inch))
+        
+        Elements.append(Paragraph(Title, styles["Heading1"]))
+        Elements.append(Spacer(2*inch, 2*inch))
+        
+        GenName='Patient'
+        PatientName=os.path.basename(file_path)[:-4]
+        Patient_Name='<para alignment="center">{0}</para>'.format(GenName+' '+PatientName)
+        Elements.append(Paragraph(Patient_Name, styles["Normal"]))
+        
+        Scan_date=datetime.datetime.fromtimestamp(os.stat(file_path).st_mtime).strftime('%Y-%m-%d')
+        Recording_datetime_text="L'enregistrement se terminant le "+Scan_date
+        Recording_datetime = '<para alignment="center">{0}</para>'.format(Recording_datetime_text)
+        Elements.append(Paragraph(Recording_datetime, styles["Normal"]))
+        Elements.append(Spacer(2*inch, 2*inch))
+        ptext = '<para alignment="center">{0}</para>'.format(formatted_time)
+        Elements.append(Paragraph(ptext, styles["Normal"]))
+        
+        
+        Elements.append(PageBreak())
+
+ 
+        for i in range(nbChannel):
+            Capteur=header(capteur[i], sep=0.1, style=CapteurStyle)
+            meanData=header('Mean='+str(np.mean(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
+            stdData=header('Standard Deviation='+str(np.std(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
+            minData=header('Minimum='+str(np.min(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
+            maxData=header('Maximum='+str(np.max(data[1:data.shape[0],i])), sep=0.1, style=ParaStyle)
+            ydata=[]
+            xdata=[]
+            data_graph=[]
+#        
+#                
+            xdata=np.arange(data.shape[0]-1)
+            ydata=data[1:data.shape[0],i]
+            N=int(np.floor(len(ydata)//100))
+            ydata_moyen=np.mean(ydata[:(len(ydata)//N)*N].reshape(-1,N), axis=1)
+            ydata_plus=[x+y for x,y in zip(ydata_moyen,np.std(ydata[:(len(ydata)//N)*N].reshape(-1,N), axis=1))]
+            ydata_moins=[x-y for x,y in zip(ydata_moyen,np.std(ydata[:(len(ydata)//N)*N].reshape(-1,N), axis=1))]
+            print(ydata)
+            data_graph = [tuple(zip(xdata,ydata_moyen)),tuple(zip(xdata,ydata_plus)),tuple(zip(xdata,ydata_moins))]
+            graph = graphout(data_graph)
+            info_capteur=[Capteur, meanData,stdData,maxData,minData,graph]
+            Elements.extend(info_capteur)
+        go(file_path)
     
             
 ## Page for the settings#####################################
@@ -448,7 +496,7 @@ def CreateSettingFile():
                 with open("SettingsFile.txt","r") as SettingFile:
                     SettingData=SettingFile.readlines()
             if not SettingData[0]:
-                optionList = ['.dat', '.csv', '.mat', '.xls']
+                optionList = ['.bin','.dat', '.csv', '.mat', '.xls']
                 with open("SettingsFile.txt","w+") as SettingFile:
                     SettingData=[SettingData[0]+newLine,".dat\n",optionList[0]+optionList[1]+optionList[2]+newLine]
                     SettingFile.writelines(SettingData)
@@ -499,90 +547,11 @@ def EditSaveLocation(self):
             break
         if fileSaveName and fileSaveName[-4:] in Extensions :
             fileNameOk=True
-            self.saveLocationText.set('File Save Name: '+fileSaveName)
+            self.saveLocationText.set(os.path.basename(fileSaveName)[:-4])
+            
         else:
             messagebox.showerror("Invalid file format", "Please check the extension or add it in the settings page.")
             break
-    
-    
-    
-    
-    
-    
-def loadDataSetup(self):
-    global load
-    Filename=loadFile()
-    setup_data=LoadData(Filename)
-    load=True
-    return setup_data
-
-def loadFile():
-    file_path = filedialog.askopenfilename()
-    return file_path
-
-def LoadData(Filename):
-    file_path=Filename
-    data = np.genfromtxt(file_path, delimiter=',')
-    fig_load=Figure(figsize=(10,7))
-    ax = fig_load.add_subplot(data.shape[1], 1, 1)
-    ax.axis([0, 1000, 0, 1])
-    ax.plot(np.arange(data.shape[0]-1),data[1:data.shape[0],0],'b')
-    ax.get_xaxis().set_visible(False)
-    fig_load.subplots_adjust(hspace = .3)
-    return[data,fig_load,ax]
-    
-    #ax=[None]*11
-#    for k in range(data.shape[1]):
-#        ax[k] = fig_load.add_subplot(data.shape[1], 1, k+1)
-#        ax[k].axis([0, 1000, 0, 1])
-#        ax[k].plot(np.arange(data.shape[0]-1),data[1:data.shape[0],k],'b')
-#        ax[k].get_xaxis().set_visible(False)
-#        if k==data.shape[1]-1:
-#            ax[k].get_xaxis().set_visible(True)
-#    fig_load.subplots_adjust(hspace = .3)
-#    graph.defineY(ax)
-#    graph.defineTitle(ax)
-#    return[data,fig_load,ax]
-    
-def PlotStartPage():
-    fig_StartPage=Figure(figsize=(10,7))
-
-    ax=[None]*11
-    for k in range(11):
-        ax[k] = fig_StartPage.add_subplot(11, 1, k+1)
-        ax[k].get_xaxis().set_visible(False)
-    graph.defineTitle(ax)
-    graph.defineY(ax)
-    fig_StartPage.subplots_adjust(hspace=0.3)
-    return[fig_StartPage,ax]
-    
-####Setup Live Plot data and update##############
-def setupP(n,fig,ttd):
-    AcqVerSav_threads.latest_data_point
-    AcqVerSav_threads.latest_data_point = np.zeros(n, dtype=int)
-    xs = np.linspace(1,1000,ttd)
-    ax=[None]*n
-    ys=[None]*n
-    line=[None]*n
-    for k in range(n):
-        ax[k] = fig.add_subplot(n, 1, k+1)
-        ys[k] = deque([0]*ttd)
-        ax[k].get_xaxis().set_visible(False)
-        
-        linek, = ax[k].plot(xs, ys[k])
-        line[k]=linek
-    graph.defineY(ax)
-    graph.defineTitle(ax)
-    fig.subplots_adjust(hspace=0.3)
-    
-    
-    
-    
-    return [ax, xs, ys,line,n]
-
-######## Save on closing window################3
-
-    
     
         
 def on_closing():
@@ -610,7 +579,6 @@ saveFrequency = 10 #in seconds
 nbIntegrityWorkers=1
 qSize = 100000
 
-a=setupP(nbChannel,fig,timeToDisplay*samplingRate)
 #ani=animation.FuncAnimation(fig, animate, frames=frameCounter, fargs=(a[0],a[1],a[2],a[3],a[4]), interval=1000/samplingRate, blit=True)
 
 #gui = Thread(target=gui_t, args=(root,), name="GUI")
